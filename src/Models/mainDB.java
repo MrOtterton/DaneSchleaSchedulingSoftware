aea04acb859b4d5c68c12a9bd67794c4ad20b0d1
Dane Schlea <dane.schlea@gmail.com>
2019-05-18 19:56:00 -0400

    Added mainDB model for database connectivity, has connect/disconnect/connection return.

---
+++ src/Models/mainDB.java
@@ -0,0 +1,48 @@
+package Models;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Shakla
+ */
+public class mainDB {
+    
+    //Database connection info
+    private static final String dbName = "U04Rc2";
+    private static final String dbURL = "jdbc:mysql://52.206.157.109" + dbName;
+    private static final String dbUser = "U04Rc2";
+    private static final String dbPass = "53688320962";
+    private static final String dbDriver = "com.mysql.jdbc.Driver";
+    private static Connection dbConn;
+    
+    //Connects to the database
+    public static void dbConnect(){
+        try{
+            Class.forName(dbDriver);
+            dbConn = DriverManager.getConnection(dbURL, dbUser, dbPass);
+        }
+        catch(SQLException e){
+            System.out.println("SQL exception");
+        } 
+        catch (ClassNotFoundException e) {
+            System.out.println("Class exception");
+        }
+    }
+    
+    //Disconnect from the batabase
+    public static void dbDisconnect(){
+        try{
+            dbConn.close();
+        } catch (SQLException e) {
+            System.out.println("SQL exception");
+        }
+    }
+    
+    //Connection return
+    public static Connection getConn(){
+        return dbConn;
+    }
+}
